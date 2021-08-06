6407be6c5d023f1d7e40c7e7e01d00a2107b5355
DAF201 <56328675+DAF201@users.noreply.github.com>
2021-08-06 13:14:29 -0500

    Create request_test.java

---
+++ request_test.java
@@ -0,0 +1,15 @@
+package Java;
+import java.net.*;
+import java.io.*;
+
+public class test {
+    public static void main(String[] args) throws Exception {
+        URL yahoo = new URL("https://example.com/");
+        URLConnection yc = yahoo.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) 
+            System.out.println(inputLine);
+        in.close();
+    }
+}
