7ca77fa7b8553b4326144f512bd097acdd272572
DAF201 <56328675+DAF201@users.noreply.github.com>
2021-12-15 15:04:23 -0600

    Create time_test.py

---
+++ time_test.py
@@ -0,0 +1,40 @@
+from time import time
+start = time()
+# extsion = ['.bmp', '.jpg', '.png', '.tif', '.gif', '.pcx', '.tga', '.exif', '.fpx', '.svg',
+#            '.psd', '.cdr', '.pcd', '.dxf', '.ufo', '.eps', '.ai', '.raw', '.WMF', '.webp', '.avif', '.apng']
+# a = '.jpg'
+# flag = False
+# for i in range(10000):
+#     if a in extsion:
+#         flag = True
+# print(flag)
+# print(time()-start)
+# iter:         0.0010471343994140625
+# list:         0.024999141693115234
+# in iter:      0.0009996891021728516
+# in list:      0.0020143985748291016
+
+
+# end = 100000
+# count = 0
+# while True:
+#     if end > count:
+#         count = count + 1
+#     else:
+#         break
+# print(time()-start)
+# while 1 +=:           0.012006044387817383
+# while True +=:        0.010997533798217773
+# while 1 a=a+1         0.01199793815612793
+# while True a=a+1      0.010999679565429688
+
+
+# for x in range(200000000):
+#     if not 0:
+#         pass
+# print(time()-start)
+# if True: 10.983509540557861
+# if 1: 10.62530255317688
+# if not False: 11.615732192993164
+# if not 0: 11.206275939941406
+
